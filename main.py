129a34d1d8bd41488e6b6e11e80f60a8e4326f49
Fred <jaco.bezuidenhout11@gmail.com>
2026-02-04 11:26:44 +1100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+#Enter Channel ID to look up here
+CHANNEL_ID = '123456789'
+
+#Use below link to register an application with twitch to create "client id" and generate a "client secret"
+#https://dev.twitch.tv/console/apps/create
+#TIP: When creating the app- for Oauth redirect URL you can use: https://localhost
+
+client_id = 'Insert Client ID here'
+client_secret = 'Insert Secret here'
+
+#Auth to get access token
+url = 'https://id.twitch.tv/oauth2/token'
+payload = {
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'grant_type': 'client_credentials',
+}
+response = requests.post(url, data=payload)
+access_token = json.loads(response.text)['access_token']
+
+#Build and make API request
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Client-Id': client_id,
+}
+url = f'https://api.twitch.tv/helix/channels?broadcaster_id={CHANNEL_ID}'
+response = requests.get(url, headers=headers)
+data = json.loads(response.text)
+
+#Return the name
+print(data['data'][0]['broadcaster_name'])
